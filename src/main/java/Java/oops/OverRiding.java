46f8357e9bcc3fb51c27101149682c09e946a74d
MD Azhar Uddin <azharuddin@STIblr.com>
2025-09-03 18:10:11 +0530

    added something

---
+++ src/main/java/Java/oops/OverRiding.java
@@ -0,0 +1,25 @@
+package Java.oops;
+class z{
+	protected Object print() {
+		System.out.println("z class method");
+		return "";
+	}
+}
+class y extends z{
+	public   Object print() {
+		return new z();
+	}
+}
+public class OverRiding extends y{
+	
+	public final z print() {
+		System.out.println("override-class method");
+		return new z();
+	}
+	
+	public static void main(String[] args) {
+		OverRiding o = new OverRiding();
+		o.print();
+	}
+
+}
